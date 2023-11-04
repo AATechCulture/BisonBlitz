bda646ca6855a9b429d4f7621de15249bc5da493
Deontae Smith <98364400+deontae-smith@users.noreply.github.com>
2023-11-04 16:14:47 -0500

    wip: Sections

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import LandingPage from './routes/LandingPage';
+import NameSection from './routes/NameSection';
+import { MilesSection } from './routes/MilesSection';
+import { TripSection } from './routes/TripSection';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<LandingPage />} />
-      {/* <Route path='/signup' element={<SignupPage />} /> */}
+      <Route path='/name-section' element={<NameSection />} />
+      <Route path='/miles-question' element={<MilesSection />} />
+      <Route path='/trip-section' element={<TripSection />} />
     </Routes>
   );
 }
---
+++ src/routes/TripSection.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { SignleQuestionForm } from '../compoents';
+
+type Props = {};
+
+const TripSection = (props: Props) => {
+  return (
+    <div className='onboarding-with-mutiple'>
+      <SignleQuestionForm
+        mainText='What is your first name?'
+        subText='We use this for your personal experince'
+        toRoute='/miles-question'
+      />
+      <SignleQuestionForm
+        mainText='What is your first name?'
+        subText='We use this for your personal experince'
+        toRoute='/miles-question'
+      />
+      <SignleQuestionForm
+        mainText='What is your first name?'
+        subText='We use this for your personal experince'
+        toRoute='/miles-question'
+      />
+    </div>
+  );
+};
+export { TripSection };
