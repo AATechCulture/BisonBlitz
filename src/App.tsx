1ed859c4d6e830af198f1b22946ead852a884188
Ivaneisha02 <90430746+Ivaneisha02@users.noreply.github.com>
2023-11-04 18:13:51 -0400

    added climate page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import LandingPage from './routes/LandingPage';
+import ClimatePage from './routes/ClimatePage';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<LandingPage />} />
       {/* <Route path='/signup' element={<SignupPage />} /> */}
+      <Route path='/climate' element={<ClimatePage />} />
     </Routes>
   );
 }
---
+++ src/routes/ClimatePage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../sass/LandingPage.scss';
+import {useNavigate} from "react-router-dom"
+ 
+const ClimatePage = () => { 
+    const navigate = useNavigate(); 
+      
+    return ( 
+        <> 
+          <h1 style={{color:"green"}}>GeeksForGeeks</h1> 
+        </> 
+    ) 
+  }; 
+export default ClimatePage;
+
+  
